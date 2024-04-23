9aabba293432868f2b89ebab2c7146510eb64a35
Ryan Collins <hello@ryd3v.com>
2024-04-22 22:48:05 -0400

    Update Ubuntu_setup.sh

--- Ubuntu_setup.sh
+++ Ubuntu_setup.sh
@@ -5,8 +5,9 @@ sudo apt purge -y apport apport-symptoms popularity-contest ubuntu-report whoops
 sudo apt autoremove -y
 
 # Update and install basic tools
-sudo apt update
-sudo apt install -y build-essential dkms gcc make perl curl wget vlc ffmpeg python3-pip git default-jre mediainfo-gui libimage-exiftool-perl mat2 subversion ripgrep jq libncurses5-dev libffi-dev gnome-tweaks transmission openjdk-17-jdk openjdk-17-jre autoconf libtool automake zip ant libde265-dev libheif-dev libpq-dev testdisk libafflib-dev libewf-dev libvhdi-dev libvmdk-dev libgstreamer1.0-0 gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio libcanberra-gtk-module bleachbit yarn openvpn tor proxychains exfat-fuse exfatprogs lm-sensors fancontrol read-edid i2c-tools
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install -y build-essential dkms gcc make perl curl wget vlc ffmpeg python3-pip git default-jre mediainfo-gui libimage-exiftool-perl mat2 subversion ripgrep jq libncurses5-dev libffi-dev gnome-tweaks transmission openjdk-17-jdk openjdk-17-jre autoconf libtool automake zip ant libde265-dev libheif-dev libpq-dev testdisk libafflib-dev libewf-dev libvhdi-dev libvmdk-dev libgstreamer1.0-0 gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio libcanberra-gtk-module bleachbit yarn openvpn tor proxychains exfat-fuse exfatprogs lm-sensors fancontrol read-edid i2c-tools gparted kleopatra
 
 # Install and configure specific tools
 sudo snap install code --classic
